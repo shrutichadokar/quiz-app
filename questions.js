c19bd980df5b86395e163162e67ef95fb596ec22
shrutichadokar <98144146+shrutichadokar@users.noreply.github.com>
2023-03-05 15:43:41 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,59 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "How many Sizes of headers are avilable in HTML by default?",
+    answer: "6",
+    options: [
+      "7",
+      "1",
+      "6",
+      "5"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which propery is used to  change the background color?",
+    answer: "background-color",
+    options: [
+      "color",
+      "bgcolor",
+      "background-color",
+      "colorbg"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Who is making the Web standards?",
+    answer: "The World Wide Web Consortium",
+    options: [
+      "The World Wide Web Consortium",
+      "Morzilla",
+      "Microsoft",
+      "Google"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which type of JavaScript language is",
+    answer: "Object-Based",
+    options: [
+      "High level",
+      "Assembly-language",
+      "Object-Oriented",
+      "Object-Based"
+    ]
+  },
+  
+];
